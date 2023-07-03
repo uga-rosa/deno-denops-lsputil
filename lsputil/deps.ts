a7ebe943f7dd745e0f871e044196f907455047a3
uga-rosa <uga6603@gmail.com>
2023-07-03 13:55:56 +0900

    Add deps.ts

---
+++ lsputil/deps.ts
@@ -0,0 +1,9 @@
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+export * as api from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
+
+export * as LSP from "npm:vscode-languageserver-types@3.17.3";
+
+// For test
+export { assertEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts";
+export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
