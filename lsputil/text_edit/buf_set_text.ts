3853bb9769470af94e860de5c268d8915374b11e
uga-rosa <uga6603@gmail.com>
2023-07-03 16:44:52 +0900

    Separate checkRange()

--- lsputil/text_edit/buf_set_text.ts
+++ lsputil/text_edit/buf_set_text.ts
@@ -3,21 +3,17 @@ import { LSPRangeError } from "./mod.ts";
 import { isPositionBefore, normalizeBufnr } from "../internal/util.ts";
 import { toUtf8Index } from "../offset_encoding/mod.ts";
 
-export async function bufSetText(
+export async function checkRange(
   denops: Denops,
   bufnr: number,
   /** utf-16 offset, 0-based */
   range: LSP.Range,
-  replacement: string[],
-): Promise<void> {
-  bufnr = await normalizeBufnr(denops, bufnr);
-
-  // Fix reversed range
-  const { start, end } = range;
-  if (!isPositionBefore(start, end)) {
-    range = { start: end, end: start };
-  }
-
+): Promise<{
+  startRow: number;
+  endRow: number;
+  startLine: string;
+  endLine: string;
+}> {
   /** 1-based */
   const startRow = range.start.line + 1;
   const startLine = (await fn.getbufline(denops, bufnr, startRow))[0];
@@ -39,9 +35,35 @@ export async function bufSetText(
     throw new LSPRangeError("end col");
   }
 
+  return { startRow, endRow, startLine, endLine };
+}
+
+export async function bufSetText(
+  denops: Denops,
+  bufnr: number,
+  /** utf-16 offset, 0-based */
+  range: LSP.Range,
+  replacement: string[],
+): Promise<void> {
+  bufnr = await normalizeBufnr(denops, bufnr);
+
+  // Fix reversed range
+  const { start, end } = range;
+  if (!isPositionBefore(start, end)) {
+    range = { start: end, end: start };
+  }
+
+  /** 1-based */
+  const {
+    startRow,
+    endRow,
+    startLine,
+    endLine,
+  } = await checkRange(denops, bufnr, range);
+
   if (denops.meta.host === "nvim") {
     const startCol = toUtf8Index(startLine, range.start.character, "utf-16");
-    const endCol = toUtf8Index(startLine, range.end.character, "utf-16");
+    const endCol = toUtf8Index(endLine, range.end.character, "utf-16");
     // 0-based
     // Extmarks will be preserved on non-modified parts of the touched lines.
     await api.nvim_buf_set_text(
@@ -56,18 +78,15 @@ export async function bufSetText(
   } else {
     // Store cursor position
     const cursor = await fn.getpos(denops, ".");
-    const source = await fn.getbufline(denops, bufnr, startRow, endRow);
     if (replacement.length === 0) {
       replacement = [
-        source[0].slice(0, range.start.character) +
-        source[source.length - 1].slice(range.end.character),
+        startLine.slice(0, range.start.character) +
+        endLine.slice(range.end.character),
       ];
     } else {
-      replacement[0] = source[0].slice(0, range.start.character) +
+      replacement[0] = startLine.slice(0, range.start.character) +
         replacement[0];
-      replacement[replacement.length - 1] += source[source.length - 1].slice(
-        range.end.character,
-      );
+      replacement[replacement.length - 1] += endLine.slice(range.end.character);
     }
     // Deleting the lines first may create an extra blank line.
     await fn.appendbufline(denops, bufnr, endRow, replacement);
