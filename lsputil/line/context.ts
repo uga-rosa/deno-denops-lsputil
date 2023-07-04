2177c465350d1154137721824db38ab21c44e618
uga-rosa <uga6603@gmail.com>
2023-07-04 15:41:45 +0900

    Add example to comment of LineContext

--- lsputil/line/context.ts
+++ lsputil/line/context.ts
@@ -2,7 +2,21 @@ import { Denops, fn } from "../deps.ts";
 
 /**
  * Context of the line where the cursor is.
+ *
  * 0-based and columns are utf-16 offset.
+ *
+ * ```typescript
+ * import { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+ * import { LineContext } from "./context.ts"
+ *
+ * export async function main(denops: Denops) {
+ *   const ctx = await LineContext.create(denops);
+ *   -- String before the cursor
+ *   const beforeLine = ctx.text.slice(0, ctx.character);
+ *   -- String after the cursor
+ *   const afterLine = ctx.text.slice(ctx.character);
+ * }
+ * ```
  */
 export class LineContext {
   character: number;
