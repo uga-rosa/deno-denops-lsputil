73d55d1ceb580440392052c922400c765610a95f
uga-rosa <uga6603@gmail.com>
2023-10-18 11:07:31 +0900

    Update deps.ts

--- lsputil/deps.ts
+++ lsputil/deps.ts
@@ -3,12 +3,22 @@ export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export * as Fn from "https://deno.land/x/denops_std@v5.0.1/function/types.ts";
 export * as api from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
 export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v5.0.1/batch/mod.ts";
 
-export * as LSP from "npm:vscode-languageserver-types@3.17.3";
+export * as LSP from "npm:vscode-languageserver-types@3.17.5";
+
+export {
+  dirname,
+  fromFileUrl,
+  isAbsolute,
+  relative,
+  toFileUrl,
+} from "https://deno.land/std@0.204.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.204.0/fs/mod.ts";
 
 // For test
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.193.0/testing/asserts.ts";
+} from "https://deno.land/std@0.204.0/testing/asserts.ts";
 export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
