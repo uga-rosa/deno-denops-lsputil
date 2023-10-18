599902334d8d3b2f6e7a06bc7dbe46a8dbead600
uga-rosa <uga6603@gmail.com>
2023-10-18 11:16:32 +0900

    Add comments for uri functions

--- lsputil/uri/mod.ts
+++ lsputil/uri/mod.ts
@@ -1,11 +1,13 @@
 import { Denops, fn, fromFileUrl, toFileUrl } from "../deps.ts";
 
+/** Gets a URI from a file path. */
 export function uriFromFname(
   fname: string,
 ): string {
   return toFileUrl(fname).toString();
 }
 
+/** Gets a URI from a bufnr. */
 export async function uriFromBufnr(
   denops: Denops,
   bufnr: number,
@@ -17,12 +19,17 @@ export async function uriFromBufnr(
   return uriFromFname(fname);
 }
 
+/** Gets a filename from a URI. */
 export function uriToFname(
   uri: string,
 ): string {
   return fromFileUrl(uri);
 }
 
+/**
+ * Gets the buffer for a uri.
+ * Creates a new unloaded buffer if no buffer for the uri already exists.
+ */
 export async function uriToBufnr(
   denops: Denops,
   uri: string,
