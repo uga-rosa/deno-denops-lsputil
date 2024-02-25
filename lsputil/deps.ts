b07f7496336fda9a3aebf8f2002837632f0ab0aa
uga-rosa <uga6603@gmail.com>
2024-02-25 09:32:17 +0900

    udd

--- lsputil/deps.ts
+++ lsputil/deps.ts
@@ -1,9 +1,9 @@
-export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
-export * as Fn from "https://deno.land/x/denops_std@v5.0.1/function/types.ts";
-export * as api from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v5.0.1/batch/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.1.0/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.1.0/function/mod.ts";
+export * as Fn from "https://deno.land/x/denops_std@v6.1.0/function/types.ts";
+export * as api from "https://deno.land/x/denops_std@v6.1.0/function/nvim/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v6.1.0/option/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v6.1.0/batch/mod.ts";
 
 export * as LSP from "npm:vscode-languageserver-types@3.17.5";
 
@@ -11,12 +11,12 @@ export {
   dirname,
   fromFileUrl,
   toFileUrl,
-} from "https://deno.land/std@0.204.0/path/mod.ts";
-export { existsSync } from "https://deno.land/std@0.204.0/fs/mod.ts";
+} from "https://deno.land/std@0.217.0/path/mod.ts";
+export { existsSync } from "https://deno.land/std@0.217.0/fs/mod.ts";
 
 // For test
 export {
   assertEquals,
   assertRejects,
-} from "https://deno.land/std@0.204.0/assert/mod.ts";
-export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
+} from "https://deno.land/std@0.217.0/assert/mod.ts";
+export { test } from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
