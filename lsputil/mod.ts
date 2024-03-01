60379a0f8a559a61f1084d0c284df27b8f9cc4f3
uga-rosa <uga6603@gmail.com>
2024-03-01 11:05:28 +0900

    assert module and LSP are exported in root mod.ts

--- lsputil/mod.ts
+++ lsputil/mod.ts
@@ -1,12 +1,15 @@
+export * from "./assert/mod.ts";
 export * from "./buffer/mod.ts";
 export * from "./cursor/mod.ts";
 export * from "./line/mod.ts";
+export * from "./lsp/mod.ts";
 export * from "./offset_encoding/mod.ts";
 export * from "./position/mod.ts";
 export * from "./range/mod.ts";
-export * from "./text_edit/mod.ts";
-export * from "./util/mod.ts";
-export * from "./lsp/mod.ts";
 export * from "./snippet/mod.ts";
+export * from "./text_edit/mod.ts";
 export * from "./uri/mod.ts";
+export * from "./util/mod.ts";
 export * from "./workspace_edit/mod.ts";
+
+export { LSP } from "./deps.ts";
